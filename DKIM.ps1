0c25109f07729836f2b6dea468eab54a79bc88db
Aaron Langham <124449717+Aaron-Langham@users.noreply.github.com>
2023-10-25 10:16:03 +0100

    Add files via upload

---
+++ DKIM.ps1
@@ -0,0 +1,34 @@
+﻿$LogonEmail = Read-Host "Enter Logon Email"
+
+Connect-ExchangeOnline -UserPrincipalName $LogonEmail
+
+
+$again = $true
+
+while ($again)
+{
+    $Option = Read-Host "
+    Enter 'g' to see current config,
+    Enter 'c' to create new config,
+    Enter 'e' to enable DKIM
+    Enter 'q' to exit"
+
+    if ($Option -eq "g")
+    {Get-DKIMSigningConfig}
+    elseif ($Option -eq "c")
+    {
+        $Domain = Read-Host "Enter Domain"
+        New-DKIMSigningConfig -Domain $Domain -Enabled $true
+        Get-DKIMSigningConfig
+    }
+    elseif ($Option -eq "e")
+    {
+        $Domain = Read-Host "Enter Domain"
+        Set-DKIMSigningConfig -Identity $Domain -Enabled $true
+        Get-DKIMSigningConfig
+    }
+    elseif ($Option -eq "q"){$again = $false}
+    else{Write-Host "Invalid Option, Try again"}
+}
+
+Disconnect-ExchangeOnline -Confirm:$false
